49216641ce675132b0f67a7fd4ffd907fc117348
Christian Grasser <christian.grasser@live.de>
2023-02-12 23:14:27 +0100

    - added missing nullpointer check - fix uninit vars

--- lexilla/lexers/LexFortran.cxx
+++ lexilla/lexers/LexFortran.cxx
@@ -354,14 +354,17 @@ static void CheckBackComLines(Accessor &styler, bool isFixFormat, Sci_Position l
 		nComCol[copyTo] = nComColB[i];
 	}
 	assert(nComL < nLines);
-	comL[nComL] = comLineCur;
-	nComCol[nComL] = nComCur;
-	for (unsigned int i=0; i<nComL; i++) {
-		unsigned copyTo = i + nComL + 1;
-		comL[copyTo]    = comLineF[i];
-		nComCol[copyTo] = nComColF[i];
+	if (nComL < nLines)
+	{
+		comL[nComL] = comLineCur;
+		nComCol[nComL] = nComCur;
+		for (unsigned int i = 0; i < nComL; i++) {
+			unsigned copyTo = i + nComL + 1;
+			comL[copyTo] = comLineF[i];
+			nComCol[copyTo] = nComColF[i];
+		}
 	}
-	
+
 	Sci_Position lineC = lineCurrent - nComL + 1;
 	Sci_PositionU iStart;
 	if (lineC <= 0) {
