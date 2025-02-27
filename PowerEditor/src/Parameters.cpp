f0feb9d9f2af04154cc39c5764c53add037a6a23
Christian Grasser <christian.grasser@live.de>
2025-02-27 18:01:50 +0100

    fix some findings likely from clang tidy

--- PowerEditor/src/Parameters.cpp
+++ PowerEditor/src/Parameters.cpp
@@ -1440,7 +1440,13 @@ bool NppParameters::load()
 		{
 			auto r = addUserDefineLangsFromXmlTree(udlDoc);
 			if (r.second - r.first > 0)
+			{
 				_pXmlUserLangsDoc.push_back(UdlXmlFileState(udlDoc, false, false, r));
+			}
+			else
+			{
+				delete udlDoc;
+			}
 		}
 	}
 
